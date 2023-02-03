2b2f7b28d14146ac0d6b5494f3ac1f6a73e1b601
Oliver Browning <lemenade3@hotmail.com>
2023-02-03 23:15:14 +0000

    placeShip function added

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,32 @@
+import Ship from "./ships";
+
 const Gameboard = () => {
-  const a = "placeholder";
+  /* make an object that contains 10 objects each subobject should 
+  have a value and then 10 sub objects, each sub object will then have 
+  a value, a hit value and a occupied value */
+
+  const board = [];
+
+  for (let i = 0; i <= 10; i += 1) {
+    const column = [];
+    for (let j = 0; j <= 10; j += 1) {
+      const cell = {
+        value: j,
+        hit: false,
+        occupied: false,
+      };
+      column.push(cell);
+    }
+    board.push(column);
+  }
+
+  const placeShip = (x, y) => {
+    board[x][y].occupied = Ship(1);
+  };
 
   return {
-    a,
+    board,
+    placeShip,
   };
 };
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -4,3 +4,37 @@ test("Gameboard is an object", () => {
   const gameboard = Gameboard();
   expect(typeof gameboard).toBe("object");
 });
+
+test("Gameboard returns an empty board", () => {
+  const gameboard = Gameboard();
+  const emptyBoard = () => {
+    let result;
+    for (let i = 0; i < 10; i += 1) {
+      for (let j = 0; j < 10; j += 1) {
+        if (gameboard.board[i][j].occupied === true) {
+          result = false;
+          return result;
+        }
+        result = true;
+      }
+    }
+    return result;
+  };
+  /* loop through each column and then loop through each cell in the column,
+  if any cell is not empty, return false, else return true */
+  expect(emptyBoard()).toBe(true);
+});
+
+test("placeShip places points at correct coordinates ", () => {
+  const gameboard = Gameboard();
+  gameboard.placeShip(3, 4);
+  gameboard.placeShip(10, 10);
+  const isOccupied = (x, y) => {
+    if (gameboard.board[x][y].occupied) {
+      return true;
+    }
+    return false;
+  };
+  expect(isOccupied(3, 4)).toBe(true);
+  expect(isOccupied(10, 10)).toBe(true);
+});
